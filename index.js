7f5711475c452374ee26272ab13f04a77c8dde4d
Mitchell Kossoris <mkossoris@gmail.com>
2019-01-18 17:50:49 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.menu-items li').click(function() {
+  const el = $(this)
+  const scrollTo = el.attr('data-scroll');
+  $('html, body').animate({
+      scrollTop: $(scrollTo).offset().top
+  }, 200);
+});
