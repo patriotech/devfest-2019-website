319bc028430247f4a471543a49626dc1b2782511
Mitchell Kossoris <mkossoris@gmail.com>
2019-01-18 21:12:06 -0600

    added about section

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $('.menu-items li').click(function() {
   const el = $(this)
   const scrollTo = el.attr('data-scroll');
   $('html, body').animate({
-      scrollTop: $(scrollTo).offset().top
+      scrollTop: $(scrollTo).offset().top - 107
   }, 700);
 });
